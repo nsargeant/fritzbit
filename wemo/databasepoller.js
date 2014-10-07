6f4866b50e0437bce8b2c4ea63c7465f24b0e623
from-nibly <jdavidson@getjive.com>
2014-10-07 15:00:44 -0600

    added poller

--- wemo/databasepoller.js
+++ wemo/databasepoller.js
@@ -1,5 +1,6 @@
 var WemoManager = require('./wemoManager.js'),
-  manager = new WemoManager('localhost', 5000);
+  manager = new WemoManager('localhost', 5000),
+  user = require('../db/user.js');
 
 
 manager.resyncDevices().done(function(data) {
@@ -10,15 +11,47 @@ manager.resyncDevices().done(function(data) {
 
 function Poller(user) {
   var interval = setInterval(function() {
-    manager.getDevices().done(function(data) {
-      for (x in data) {
 
+    user.getUser(user, function(err, user) {
+      if (!user.wemo) {
+        user.wemo = {};
+        user.wemo.used = 0;
       }
-    }).fail(function() {
+      user.wemo.earned = user.info.sumary.steps / user.website.ratio;
+      if (user.wemo.earned <= user.wemo.used) {
 
+        manager.getDevices().done(function(data) {
+          for (x in data) {
+            manager.toggleDevice(x, 'off').fail(function(err) {
+              console.log('oh no there was an error toggling the device');
+            });
+          }
+        }).fail(function(err) {
+          console.log('OH NO THERE WAS AN ERROR GETTING DEVICES!', err);
+        });
+      } else {
+        console.log('incrementing used credits from %s to %s', used, used + 1);
+        user.wemo.used += 1;
+        console.log('saving user to database');
+
+        user.save(function(err, data) {
+          if (err) {
+            console.log('there was an error saving to the database', err);
+          }
+        });
+
+        manager.getDevices().done(function(data) {
+          for (x in data) {
+            manager.toggleDevice(x, 'on').fail(function(err) {
+              console.log('oh no there was an error toggling the device');
+            });
+          }
+        }).fail(function(err) {
+          console.log('OH NO THERE WAS AN ERROR GETTING DEVICES!', err);
+        });
+      }
     });
   }, 60 * 1000);
-
   this.destroy = function() {
     clearInterval(interval);
   };
